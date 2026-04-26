aa049d4bb8fa945312e7cf9b41a23205d027cb2b
Flash Cabuloso <thurluetkemeyer@gmail.com>
2026-04-26 12:06:21 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="dash_exporter",
-    version="1.0.0",
+    version="0.1",
     packages=find_packages(),
-    install_requires=[
-        "discord.py>=2.4.0",
-        "pytz>=2024.1",
-    ],
 )
